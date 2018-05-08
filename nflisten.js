801e28de3bd8d97ba22769b2b235082dba801f1c
Stephen Kelly <stevekm@users.noreply.github.com>
2018-05-07 22:56:50 -0400

    start MySQL app connection

--- nflisten.js
+++ nflisten.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+// var process = require('process'); // process.pid
+var mysql = require('mysql');
+
+// Configure MySQL connection
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'nflistener',
+	password: 'nflistener',
+	database: 'nflistener'
+  })
+
+//Establish MySQL connection
+connection.connect(function(err) {
+   if (err)
+      throw err
+   // else {
+   //     console.log('Connected to MySQL');
+       // Start the app when connection is ready
+       // app.listen(3000);
+       // console.log('Server listening on port 3000');
+ }
+});
 
 // parse application/json
 app.use(bodyParser.json())
