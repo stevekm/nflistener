af96887408acb5df58e26463efb3e42f4bb66cac
Stephen Kelly <stevekm@users.noreply.github.com>
2018-05-08 00:46:07 -0400

    start db setup in script

--- nflisten.js
+++ nflisten.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
-// var process = require('process'); // process.pid
+var process = require('process'); // process.pid
 var mysql = require('mysql');
+const port = 5000;
+const tablename = 't' + port.toString() + process.pid.toString()
 
 // Configure MySQL connection
 var connection = mysql.createConnection({
@@ -16,24 +18,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
    if (err)
       throw err
-   // else {
-   //     console.log('Connected to MySQL');
+   else {
+       console.log('>>> Connected to MySQL');
+
+    var sql = "CREATE TABLE " + tablename + " (id INT AUTO_INCREMENT PRIMARY KEY)";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(">>> Table " + tablename + " created");
+    });
+
        // Start the app when connection is ready
-       // app.listen(3000);
-       // console.log('Server listening on port 3000');
- }
+       app.listen(port);
+       console.log('>>> Server listening on port 3000');
+    }
 });
 
 // parse application/json
 app.use(bodyParser.json())
 
 app.post('/', function(req, res) {
+    var jsondata = req.body;
+    var keys = [];
+    var values = [];
+
+    // get all the keys in the JSON
+    for(var key in jsondata) keys.push(key);
+
+    // get all values
+    for(var i=0; i< keys.length; i++) values.push(jsondata[keys[i]])
+
    // print to console
-   console.log(req.body);
+   // console.log(jsondata);
+   console.log(JSON.stringify(jsondata, null));
 
    // just call res.end(), or show as string on web
-   res.send(JSON.stringify(req.body, null, 4));
+   res.send(JSON.stringify(jsondata, null, 4));
 });
-
-const port = 5000;
-app.listen(port);
